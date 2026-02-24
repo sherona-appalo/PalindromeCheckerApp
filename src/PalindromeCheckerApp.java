29d78037432b858d2dd215021e6948908ee55e00
sherona-appalo <amsheronaappalo@gmail.com>
2026-02-24 14:29:43 +0530

    UC7: Deque-Based Optimized Palindrome Checker

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "refer";
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) {
+            deque.add(c);
+        }
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
